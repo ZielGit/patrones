7f0c5b454f6119080dd939bbb6210039f341e0da
Frans <frans_421_12@hotmail.com>
2025-06-22 18:42:22 -0500

    initial commit

---
+++ app/solid/Prueba.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\solid;
+
+class Prueba
+{
+    public function saludar()
+    {
+        echo "Hola desde la clase Prueba";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use App\solid\Prueba;
+
+$prueba = new Prueba();
+$prueba->saludar();
